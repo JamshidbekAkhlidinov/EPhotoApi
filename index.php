19cadaa26f73d2e8518d082d1da4fbb23155ea40
JamshidbekAkhlidinov <JamshidbekAkhlidinov@gmail.com>
2023-03-08 10:14:06 +0500

    updated

--- index.php
+++ index.php
@@ -14,6 +14,11 @@ if(isset($_GET['code']) && (isset($_GET['name1']) || isset($_GET['photo1']))){
    if(isset($_GET['name2'])){
       $text[] = $_GET['name2'];
    }
+   if(isset($_GET['radio0'])){
+      $radio0 = $_GET['radio0'];
+   }else{
+      $radio0 = '';
+   }
    if(isset($_GET['photo1'])){
       $name = 'img/'.rand(10000,99999).'.'.strrev(explode('.',strrev($_GET['photo1']))[0]);
       file_put_contents($name, file_get_contents($_GET['photo1']));
@@ -28,7 +33,7 @@ if(isset($_GET['code']) && (isset($_GET['name1']) || isset($_GET['photo1']))){
       'text' => $text,
       'image' => $photo,
        'radio0' => [
-           'radio' => 'f469ed11-98c3-4b29-997e-68e9460faf6b', // Call EPhoto360::getEffect() method to see the list of filter.
+           'radio' => $radio0,
        ]
       ]);
 
